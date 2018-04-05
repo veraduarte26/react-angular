e13f50c5f2ecbe8117c2ed78ced6b7e5c7398a50
Vera Lúcia Domingos Duarte <vera.algarve@gmail.com>
2018-04-05 10:31:31 +0100

    environment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import env from "@env";
 
 // import './style.sass';
 
-const App = () => (
+const App = () => {
+    console.log('env', env);           
+        return (   
     <div>
         <h1>Hello, world!</h1>
     </div>
-);
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
